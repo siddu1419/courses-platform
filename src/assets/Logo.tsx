eb3349779e63a7160eb34eb938ecdd2bbb79154f
Siddhartha <siddharthaadirinti@changejar.in>
2025-03-26 12:25:59 +0530

    Updated logo

--- src/assets/Logo.tsx
+++ src/assets/Logo.tsx
@@ -1,11 +1,11 @@
-import logo from '../assets/Exam Trakker Final File 06.jpg';
+import logo from '../assets/Exam-Trakker-Logo.jpg';
 
 export default function Logo() {
   return (
     <img
       src={logo}
       alt="Logo"
-      className="h-[52px] w-auto max-w-[180px] object-contain"
+      className="h-[64px] w-auto max-w-[200px] object-contain"
     />
   );
 }
